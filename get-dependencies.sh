1b8733cd9e451dff29eefcdd2c43f3265a24e079
Link4Electronics <link4electronics@gmail.com>
2026-04-10 08:28:18 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,36 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	fluidsynth		   \
+	hicolor-icon-theme \
+	libsamplerate	   \
+	pipewire-audio 	   \
+	pipewire-jack  	   \
+	sdl2_mixer	   	   \
+	sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building Crispy Doom..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/fabiangreffrath/crispy-doom"
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of Crispy Doom..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./crispy-doom
+else
+	echo "Making stable build of Crispy Doom..."
+	VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | grep 'refs/tags/crispy-doom-[0-9]' | grep -v '\^{}' | tail -n1 | sed 's/.*\///; s/^crispy-doom-//')"
+	git clone --branch "crispy-doom-$VERSION" --single-branch --depth 1 "$REPO" ./crispy-doom
+fi
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cd ./crispy-doom
+autoreconf -fi
+./configure --prefix=/usr
+make -j$(nproc)
+make install
